bc7e80d88cab36c8b150990a3bbf8415d36cefd4
Esha Patel <Patellesha11@gmail.com>
2025-03-27 20:54:56 +0300

    added component

--- react-api/src/RandomUser.js
+++ react-api/src/RandomUser.js
@@ -2,6 +2,8 @@ import react, {useState, useEffect} from "react";
 
 
 function RandomUser () {
+
+    const [user, setUser] = useState("");
     return (
         <div>
             <h1>Random User</h1>
