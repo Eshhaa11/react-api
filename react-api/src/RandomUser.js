66d981d5ac468156b39b9a5986e8973f6d4621f9
Esha Patel <Patellesha11@gmail.com>
2025-03-27 21:02:17 +0300

    added json response

--- react-api/src/RandomUser.js
+++ react-api/src/RandomUser.js
@@ -8,6 +8,7 @@ function RandomUser () {
     const fetchUser = async () => {
         try {
             const response = await fetch ('https://randomuser.me/api/');
+            const data = await response.json();
         }
 
     }
