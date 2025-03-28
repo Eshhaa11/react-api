b2b7968b6b2659ab86ca40badc21427422659374
Esha Patel <Patellesha11@gmail.com>
2025-03-28 16:31:15 +0300

    add

--- react-api/src/RandomUser.js
+++ react-api/src/RandomUser.js
@@ -2,34 +2,17 @@ import react, {useState, useEffect} from "react";
 
 
 function RandomUser () {
-
-    const [user, setUser] = useState("null");
-
     const fetchUser = async () => {
-        try {
-            const response = await fetch ('https://randomuser.me/api/');
-            const data = await response.json();
-            console.log(data);
-            setUser(data.results[0]);
-        } catch(error) {
-            console.log("ërror fetching data", error);
-
-        };
-
+    
         useEffect(() => {
             fetchUser(); 
         }, []); 
 
     }
+    
     return (
         <div>
             <h1>Random User</h1>
-            {user && (
-            <div>
-                <p>Name: {user.name.first} {user.name.last}</p>
-                <p>Email: {user.email}</p>
-            </div>
-        )}
 
         </div>
     );
